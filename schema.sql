14f807a5b6c6144c4291263780a1315cdeb34105
Zain Sadaqat <zain.sadaqet@gmail.com>
2021-12-08 01:17:28 +0500

    Create database and table

--- schema.sql
+++ schema.sql
@@ -1,5 +1,11 @@
 /* Database schema to keep the structure of entire database. */
+CREATEDB VET_CLINIC;
 
-CREATE TABLE animals (
-    name varchar(100)
+CREATE TABLE ANIMALS (
+    ID INTEGER,
+    NAME VARCHAR(100),
+    DATE_OF_BIRTH DATE,
+    ESCAPE_ATTEMPTS INTEGER,
+    NEUTERED BOOLEAN,
+    WEIGHT_KG FLOAT
 );
